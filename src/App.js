86ca77639b3bc5b6724db297e684223d85be92fe
eylonshm <eylonshm@gmail.com>
2022-10-07 16:31:45 +0300

    Initialize MF container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import HomePage from './components/HomePage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
+const Header = () => (
+  <div>
+    <Link to='/'>home</Link>
+    <br />
+    <Link to='/react'>use react</Link>
+  </div>
+)
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <hr />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
---
+++ src/components/HomePage/index.js
@@ -0,0 +1,12 @@
+import { mount } from 'helloReact/HelloReactApp'
+import React, { useRef, useEffect } from 'react'
+
+export default () => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    mount(ref.current)
+  }, [])
+
+  return <div ref={ref} />
+}
